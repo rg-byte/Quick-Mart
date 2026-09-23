55a2b4ffe86fdd9fb83950033e38c21ce5c5cd78
rg-byte <rhythmgarg890@gmail.com>
2026-09-23 14:22:54 +0530

    modified vite.config.js

--- Frontend/vite.config.js
+++ Frontend/vite.config.js
@@ -1,7 +1,16 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import path from 'path'
 
-// https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  build: {
+    rollupOptions: {
+      input: {
+        main: path.resolve(__dirname, 'index.html'),
+        home: path.resolve(__dirname, 'src/Home/home.html'),
+        cart: path.resolve(__dirname, 'src/Cart/cart.html'),
+      }
+    }
+  }
 })
